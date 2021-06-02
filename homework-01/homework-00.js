0e9101ee194008cf54af013d5c003f85cdfd195d
Bohdan Omelianets <omelianecb@gmail.com>
2021-06-02 22:59:27 +0300

    code refactoring

--- homework-01/homework-00.js
+++ homework-01/homework-00.js
@@ -1,34 +1,39 @@
-const BREAD = 15.678,
-      CHEESE = 123.965,
-      MILK = 90.2345;
-const CLIENT_MONEY = 500;
-let date = new Date().toDateString();
+const bread = 15.678,
+      cheese = 123.965,
+      milk = 90.2345;
+const clientMoney = 500;
+const date = new Date().toDateString();
 
 //Basic
 
-let maximum = Math.max(BREAD, CHEESE, MILK);
-let minimum = Math.min(BREAD, CHEESE, MILK);
-let summOfProducts = BREAD + CHEESE + MILK;
-let summOfProductsFloor = Math.floor(BREAD) + Math.floor(CHEESE) + Math.floor(MILK);
-let discount = Math.random();
-let priceWithDiscount = summOfProducts - (summOfProducts * discount);
-let profit = priceWithDiscount - (summOfProducts / 2);
+const maxProductsPrice = Math.max(bread, cheese, milk);
+const minProductsPrice = Math.min(bread, cheese, milk);
+const summOfProducts = bread + cheese + milk;
+const summOfProductsFloor = Math.floor(bread) + Math.floor(cheese) + Math.floor(milk);
+const summProductsRounded = (Math.round(summOfProductsFloor / 100)) * 100;
+const isEven = summOfProductsFloor % 2 === 0;
+const clientChange = (clientMoney - summOfProducts).toFixed(2);
+const averageAmount = ((bread + cheese + milk) / 3).toFixed(2);
+const discount = Math.random();
+const discountInCash = (discount * 100).toFixed(0);
+const priceWithDiscount = (summOfProducts - (summOfProducts * discount)).toFixed(2);
+const profit = (priceWithDiscount - (summOfProducts / 2)).toFixed(2);
 
 //Advanced
 
 document.querySelector('.price-counter').innerHTML = 
 (`<ol>
-    <li>Найбільше число: <b>${maximum}</b> грн</li>
-    <li>Найменше число: <b>${minimum}</b> грн</li>
+    <li>Найбільше число: <b>${maxProductsPrice}</b> грн</li>
+    <li>Найменше число: <b>${minProductsPrice}</b> грн</li>
     <li>Сума всіх товарів: <b>${summOfProducts}</b> грн</li>
     <li>Сума всіх товарів округлена: <b>${summOfProductsFloor}</b> грн</li>
-    <li>Сума округлена до сотень: <b>${(Math.round(summOfProductsFloor / 100)) * 100}</b> грн</li>
-    <li>Чи парне число: <b>${summOfProductsFloor % 2 == 0}</b></li>
-    <li>Решта з 500 грн складає: <b>${(CLIENT_MONEY - summOfProducts).toFixed(2)}</b> грн</li>
-    <li>Середнє значення цін: <b>${((BREAD + CHEESE + MILK) / 3).toFixed(2)}</b> грн</li>
-    <li>Ваша знижка складає: <b>${(discount * 100).toFixed(0)}%</b></li>
-    <li>Cума зі знижкою: <b>${priceWithDiscount.toFixed(2)}</b> грн</li>
-    <li>Чистий прибуток складає: <b>${profit.toFixed(2)}</b> грн</li>
+    <li>Сума округлена до сотень: <b>${summProductsRounded}</b> грн</li>
+    <li>Чи парне число: <b>${isEven}</b></li>
+    <li>Решта з 500 грн складає: <b>${clientChange}</b> грн</li>
+    <li>Середнє значення цін: <b>${averageAmount}</b> грн</li>
+    <li>Ваша знижка складає: <b>${discountInCash}%</b></li>
+    <li>Cума зі знижкою: <b>${priceWithDiscount}</b> грн</li>
+    <li>Чистий прибуток складає: <b>${profit}</b> грн</li>
     </ol>
     <hr>
     <div class="date">${date}</div>`);
---
+++ homework-02/homework-02.js
@@ -0,0 +1,34 @@
+// const BREAD = 15.678,
+//       CHEESE = 123.965,
+//       MILK = 90.2345;
+// const CLIENT_MONEY = 500;
+// let date = new Date().toDateString();
+
+// //Basic
+
+// let maximum = Math.max(BREAD, CHEESE, MILK);
+// let minimum = Math.min(BREAD, CHEESE, MILK);
+// let summOfProducts = BREAD + CHEESE + MILK;
+// let summOfProductsFloor = Math.floor(BREAD) + Math.floor(CHEESE) + Math.floor(MILK);
+// let discount = Math.random();
+// let priceWithDiscount = summOfProducts - (summOfProducts * discount);
+// let profit = priceWithDiscount - (summOfProducts / 2);
+
+// //Advanced
+
+// document.querySelector('.price-counter').innerHTML = 
+// (`<ol>
+//     <li>Найбільше число: <b>${maximum}</b> грн</li>
+//     <li>Найменше число: <b>${minimum}</b> грн</li>
+//     <li>Сума всіх товарів: <b>${summOfProducts}</b> грн</li>
+//     <li>Сума всіх товарів округлена: <b>${summOfProductsFloor}</b> грн</li>
+//     <li>Сума округлена до сотень: <b>${(Math.round(summOfProductsFloor / 100)) * 100}</b> грн</li>
+//     <li>Чи парне число: <b>${summOfProductsFloor % 2 == 0}</b></li>
+//     <li>Решта з 500 грн складає: <b>${(CLIENT_MONEY - summOfProducts).toFixed(2)}</b> грн</li>
+//     <li>Середнє значення цін: <b>${((BREAD + CHEESE + MILK) / 3).toFixed(2)}</b> грн</li>
+//     <li>Ваша знижка складає: <b>${(discount * 100).toFixed(0)}%</b></li>
+//     <li>Cума зі знижкою: <b>${priceWithDiscount.toFixed(2)}</b> грн</li>
+//     <li>Чистий прибуток складає: <b>${profit.toFixed(2)}</b> грн</li>
+//     </ol>
+//     <hr>
+//     <div class="date">${date}</div>`);
