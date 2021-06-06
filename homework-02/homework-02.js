f51b1d6ace9324ef7a6582b3ff447f3e5ff1df15
Bohdan Omelianets <omelianecb@gmail.com>
2021-06-06 11:22:45 +0300

    refactoring

--- homework-02/homework-02.js
+++ homework-02/homework-02.js
@@ -1,31 +1,26 @@
-let firstNumber;
-let secondNumber;
-let skipEven;
-let summOfNumbers = 0;
-
 function numberCounter() {
+    let firstNumber;
+    let secondNumber;
+    let skipEven;
+    let summOfNumbers = 0;
     // Просимо ввести число до тих пір поки користувач введе нормальне число.
     do {
         firstNumber = +prompt('Enter first number:', '0');
         secondNumber = +prompt('Enter second number:', '0');
     } while ((isNaN(firstNumber) || isNaN(secondNumber)) ||
-            (firstNumber !== parseInt(firstNumber) || secondNumber !== parseInt(secondNumber)));
+        (firstNumber !== parseInt(firstNumber) || secondNumber !== parseInt(secondNumber)));
 
     skipEven = confirm('Do you want to skip even numbers?');
-    // Визначаємо менше і більше число.
+    // Визначаємо менше і більше число щоб підставити у цикл в правильному порядку.
     let minNumber = Math.min(firstNumber, secondNumber);
     let maxNumber = Math.max(firstNumber, secondNumber);
-    
+
     // Перебираємо всі числа у відповідному діапазоні і перевіряємо на парність.
     for (minNumber; minNumber <= maxNumber; minNumber++) {
-        if (skipEven) {
-            if (minNumber % 2 === 0) {
+        if (skipEven && minNumber % 2 === 0) {
                 continue;
             }
             summOfNumbers += minNumber;
-        } else {
-            summOfNumbers += minNumber;
-        }
     }
     document.querySelector('.number-container').innerHTML = summOfNumbers;
 
