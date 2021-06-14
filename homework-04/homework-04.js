3ca98b0e067ca8fafb7720a7ee70e5bfebd52e68
Bohdan Omelianets <omelianecb@gmail.com>
2021-06-14 15:08:05 +0300

    rewrite homework 4

--- homework-04/homework-04.js
+++ homework-04/homework-04.js
@@ -7,9 +7,9 @@
  // У вас повинен вийти вкладений масив з парами студентів: [["Олександр", "Олена"], [..], [...]];
  function getPairs(arr) {
      const studentsPairs = [];
-     studentsPairs.push( [arr[0], arr[3] ] );
-     studentsPairs.push( [arr[1], arr[2] ] );
-     studentsPairs.push( [arr[4], arr[5] ] );
+     studentsPairs.push([arr[0], arr[3]]);
+     studentsPairs.push([arr[1], arr[2]]);
+     studentsPairs.push([arr[4], arr[5]]);
      return studentsPairs;
  }
  console.log('Function 1', getPairs(students));
@@ -19,37 +19,58 @@
  // Зіставте пари з попереднього завдання та теми проєктів, над якими студенти будуть працювати.
  // Повинен вийти вкладений масив виду: [["Олександр і Олена", "Теорія автоматів"], [...], [...]]
  function devideThemes() {
-     const group = [];
-     const pairs = getPairs(students);          // Отримуємо багаторівневий масив з попередньої функції.
-     pairs.forEach((item, index) => {           // Перебираємо масив з парами.
-         item = item.join(' і ');               // Кожний вкладений масив петворюємо в рядок типу - "Олександр і Олена".
-         group.push( [ item, themes[index] ] ); // В пустий масив пушимо масив з двох елементів, рядка і теми.
-     });
-     return group;
+     // map при кожній ітерації повертає масив з рядком типу - "Олександр і Олена" і новою темою.
+     // в кінці функція повертає масив з цими підмасивами.
+     const pairs = getPairs(students);
+     return pairs.map((pair, index) => [pair.join(' i '), themes[index]]);
+
+     //  const group = [];
+     //  const pairs = getPairs(students);          // Отримуємо багаторівневий масив з попередньої функції.
+     //  pairs.forEach((item, index) => {           // Перебираємо масив з парами.
+     //      item = item.join(' і ');              // Кожний вкладений масив петворюємо в рядок - "Олександр і Олена".
+     //      group.push( [ item, themes[index] ] ); // В пустий масив пушимо масив з двох елементів, рядка і теми.
+     //  });
+     //  return group;
  }
  console.log('Function 2', devideThemes());
 
 
  // Завдання 3
  // Зіставте оцінки(marks) зі студентом(students): [["Саша", 4], [...], [...]]
- function setMarks(arr) {
-     const markedStudents = [];
-     arr.forEach((item, index) => {                     // Перебираємо масив зі студентів
-         markedStudents.push( [ item, marks[index] ] ); // та кожний елемент пушим в новий масив разом з оцінкою.
-     });
-     return markedStudents;
+ function setMarks(studentsArr) {
+     return studentsArr.map((student, index) => [student, marks[index]]);
  }
  console.log('Function 3', setMarks(students));
 
+ 
  // Завдання 4
  // Поставте кожній парі випадкову оцінку(від 1 до 5) за проєкт(тут функція буде нечистою,
  // але не повинна мутувати массив): [["Олександр і Олена", "Теорія автоматів", 5], [...], [...]]
  function randomMark() {
-    const markedGroup = devideThemes();           // Отримуємо масив з 2-ї функції.
-    markedGroup.forEach((item) => {                 
-        item.push(Math.ceil(Math.random() * 5) );    // Перебираємо масив та пушимо у підмасив рандомну оцінку. 
-    });
-    return markedGroup;
+     const studentsGroup = devideThemes();
+     return studentsGroup.map(item => [...item, Math.ceil(Math.random() * 5)]);
  }
  console.log('Function 4', randomMark());
 
+ document.body.innerHTML = `<div style="color:white; padding:50px">
+    <div style="text-align:center">${JSON.stringify(getPairs(students))}</div><br>
+    <div style="text-align:center">${JSON.stringify(devideThemes() )}</div><br>
+    <div style="text-align:center">${JSON.stringify(setMarks(students))}</div><br>
+    <div style="text-align:center">${JSON.stringify(randomMark())})</div>
+ </div>`;
+
+
+ //  Мутують початковий масив
+ //  sort()
+ //  pop()
+ //  push()
+ //  shift()
+ //  unshift()
+ //  forEach()
+
+
+ //  Не мутують початковий масив
+ //  slice()
+ //  join()
+ //  split()
+ //  map()
