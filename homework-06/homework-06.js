997764a7df8f0470024505098cc916978512361e
Bohdan Omelianets <omelianecb@gmail.com>
2021-06-21 19:25:09 +0300

    refresh homework 6

--- homework-06/homework-06.js
+++ homework-06/homework-06.js
@@ -2,27 +2,27 @@ const students = [{
     name: "Tanya",
     course: 3,
     subjects: {
-      math: [4, 4, 3, 4],
-      algorithms: [3, 3, 3, 4, 4, 4],
-      data_science: [5, 5, 3, 4]
+        math: [4, 4, 3, 4],
+        algorithms: [3, 3, 3, 4, 4, 4],
+        data_science: [5, 5, 3, 4]
     }
-  }, {
+}, {
     name: "Victor",
     course: 4,
     subjects: {
-      physics: [5, 5, 5, 3],
-      economics: [2, 3, 3, 3, 3, 5],
-      geometry: [5, 5, 2, 3, 5]
+        physics: [5, 5, 5, 3],
+        economics: [2, 3, 3, 3, 3, 5],
+        geometry: [5, 5, 2, 3, 5]
     }
-  }, {
+}, {
     name: "Anton",
     course: 2,
     subjects: {
-      statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
-      english: [5, 3],
-      cosmology: [5, 5, 5, 5]
+        statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
+        english: [5, 3],
+        cosmology: [5, 5, 5, 5]
     }
-  }];
+}];
 
 // Function 1
 function getSubjects(student) {
@@ -37,30 +37,30 @@ function getSubjects(student) {
         const modifiedSubjects = [];
 
         studentSubjects.forEach((subject) => {
-            modifiedSubjects.push( setFirstLetterUpper(subject) ); 
+            modifiedSubjects.push(setFirstLetterUpper(subject));
         });
-            
+
         return modifiedSubjects;
     } else {
 
         return 'No matches';
     }
 }
-console.log('Список всіх предметів: ', getSubjects(students[0]) );
+console.log('Список всіх предметів: ', getSubjects(students[0]));
 
 
 // Function 2
 function getAverageMark({subjects}) {
     const marks = [];
 
-    for ( let key in subjects) {
-        marks.push( ...subjects[key] );
+    for (let key in subjects) {
+        marks.push(...subjects[key]);
     }
 
     return getAverage(...marks).toFixed(2);
 }
 
-console.log('Середня оцінка студента: ', getAverageMark(students[0]) );
+console.log('Середня оцінка студента: ', getAverageMark(students[0]));
 
 
 // Function 3
@@ -71,8 +71,8 @@ function getStudentInfo(student) {
     studentInfo.averageMark = +getAverageMark(student);
 
     return studentInfo;
-}    
-console.log('Інформація про студента: ', getStudentInfo(students[0]) );
+}
+console.log('Інформація про студента: ', getStudentInfo(students[0]));
 
 
 // Function 4
@@ -85,7 +85,7 @@ function getStudentsNames(students) {
 
     return studentNames.sort();
 }
-console.log('Список всіх студентів: ', getStudentsNames(students) );
+console.log('Список всіх студентів: ', getStudentsNames(students));
 
 
 // Function 5
@@ -94,6 +94,7 @@ function getBestStudent(students) {
     let bigestMark = 0;
 
     students.forEach(obj => {
+        
         if (getAverageMark(obj) > bigestMark) {
             bigestMark = getAverageMark(obj);
             bestStudent = obj.name;
@@ -102,7 +103,7 @@ function getBestStudent(students) {
 
     return bestStudent;
 }
-console.log('Найкращий студент: ', getBestStudent(students) );
+console.log('Найкращий студент: ', getBestStudent(students));
 
 
 // Function 6
@@ -112,16 +113,16 @@ function calculateWordLetters(str) {
     for (let i = 0; i < str.length; i++) {
         const regExp = new RegExp(str[i], 'gi');
 
-        if(str[i] in strangeObject) {
+        if (str[i] in strangeObject) {
             continue;
         }
 
-        strangeObject[ str[i] ] = str.match(regExp).length;
+        strangeObject[str[i]] = str.match(regExp).length;
     }
 
     return strangeObject;
 }
-console.log('Об\'єкт з букв переданого слова: ', calculateWordLetters('test') );
+console.log('Об\'єкт з букв переданого слова: ', calculateWordLetters('test'));
 
 
 
@@ -142,3 +143,88 @@ function getAverage(...rest) {
         return (prev + current);
     }, 0) / rest.length;
 }
+
+
+// Tabs
+const tabcontent = document.getElementsByClassName("tabcontent");
+const tablinks = document.getElementsByClassName("tablinks");
+const tab = document.querySelector(".tab");
+const result = document.querySelector('.result');
+
+function hideContent() {
+
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    result.textContent = '';
+}
+
+function showContent(index = 0) {
+    tablinks[index].classList.add("active");
+    tabcontent[index].style.display = "block";
+}
+
+hideContent();
+showContent();
+
+tab.addEventListener('click', (event) => {
+
+    Array.from(tablinks).forEach((item, i) => {
+
+        if (item === event.target) {
+            hideContent();
+            showContent(i);
+        }
+    });
+});
+
+// 
+const selectElement = document.querySelectorAll('.form-control');
+
+selectElement[0].addEventListener('change', (event) => {
+    if (event.target.value) {
+        result.textContent = getSubjects(students[event.target.value]);
+    } else {
+        result.textContent = "";
+    }
+});
+
+selectElement[1].addEventListener('change', (event) => {
+    if (event.target.value) {
+        result.textContent = getAverageMark(students[event.target.value]);
+    } else {
+        result.textContent = "";
+    }
+});
+
+selectElement[2].addEventListener('change', (event) => {
+    if (event.target.value) {
+        result.textContent = JSON.stringify(getStudentInfo(students[event.target.value]));
+    } else {
+        result.textContent = "";
+    }
+});
+
+selectElement[3].addEventListener('change', (event) => {
+    if (event.target.value) {
+        result.textContent = getStudentsNames(students);
+    } else {
+        result.textContent = "";
+    }
+});
+
+selectElement[4].addEventListener('change', (event) => {
+    if (event.target.value) {
+        result.textContent = getBestStudent(students);
+    } else {
+        result.textContent = "";
+    }
+});
+
+selectElement[5].addEventListener('input', (event) => {
+    result.textContent = JSON.stringify(calculateWordLetters(event.target.value));
+});
