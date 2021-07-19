5dd6fa6ba1d55300f48234795497e6b97eee89dd
Bohdan Omelianets <omelianecb@gmail.com>
2021-07-19 12:27:55 +0300

    refresh code

--- homework-13/homework-13.js
+++ homework-13/homework-13.js
@@ -1,212 +1,113 @@
-const WOOKIEE = '/?format=wookiee';
-const root = document.querySelector('#root'); // Елемент в який рендеряться дані
+const root = document.querySelector('#root');
 const load = document.querySelector('.load');
 
-// Функції для зручності отримання даних
-/////////////////////////////////////////
-async function getPage(url) {
-    return fetch(url).then(data => {
-        if(data.status !== 200) {
-            return null;
-        } else {
-            return data.json();
-        }
-    });
+async function getJSONResponse(url) {
+  return fetch(url).then(data => {
+
+    if (data.status !== 200) {
+      throw new Error(data.status);
+    } else {
+      return data.json();
+    }
+  });
 }
-
-async function getElementOf(element, sequenceNumber = 1) {
-    return getPage(`https://swapi.dev/api/${element}/${sequenceNumber}`);
-
-}
-
-async function getPageOf(elements, page) {
-    return getPage(`https://swapi.dev/api/${elements}/?page=${page}`);
-}
-
-async function getAllElementsOf(staff) {
-    let arr = [];
-
-    getPage(`https://swapi.dev/api/${staff}`)
-        .then(response => {
-            let count = response.count / 10 + 1;
-            for (let i = 1; i < count; i++) {
-                getPageOf(staff, i)
-                    .then(res => {
-                        arr.push(...res.results);
-                    });
-            }
-        });
-    arr = await Promise.all(arr);
-    return arr;
-}
-
-////////////////////////////////////////////
-// Створення функції як рендерить дані на сторінку
-
-let curPage = null;
-let prevPage = null;
-
-async function render(page) {
-        load.style.display = 'block';
-
-    
-
-    getPage(page)
-        .then(res => {
-    load.style.display = 'none';
-
-            root.textContent = '';
-            curPage = res.next;
-            prevPage = res.previous;
-
-            res.results.map(item => {
-                let div = document.createElement('div');
-                div.classList.add('item');
-                let h2 = document.createElement('h2');
-                let p1 = document.createElement('p');
-                let p2 = document.createElement('p');
-                const img =document.createElement('img');
-
-                if(page.indexOf('people') !== -1) {    
-                    const id = parseInt((item.url).replace(/\D+/g,""));
-                    img.setAttribute('src', `./img/${id}.jpg`);
-                    img.setAttribute('alt', 'avatar');
-                    img.setAttribute('class', 'avatar');
-                    // div.append(img);
-                }
-                // const img =document.createElement('img');
-                if(page.indexOf('films') !== -1) {
-                    const id = parseInt((item.url).replace(/\D+/g,""));
-                    img.setAttribute('src', `./img/film${id}.jpg`);
-                    img.setAttribute('alt', 'poster');
-                    img.setAttribute('class', 'poster');
-                    // div.append(img);
-                }
-
-                h2.textContent = item.title || item.name;
-
-                p1.textContent = item.birth_year || item.climate || item.classification ||
-                    item.starship_class || item.vehicle_class || item.release_date.slice(0, 4);
-
-                p2.textContent = item.director || item.diameter ||
-                    item.language || item.max_atmosphering_speed ||
-                    (item.gender === 'male' ? String.fromCodePoint('0x2642') :
-                        item.gender === 'female' ? String.fromCodePoint('0x2640') :
-                        String.fromCodePoint('0x26A7'));
-
-                div.append(h2, img, p1, p2);
-                root.append(div);
-            });
-        });
-}
-/////////////////////////////////////////
-
-//  Показати інфо про персонажів вибраного фільму
-// Завдання 1, 4
+////////////////////////////
 
 const getInfoBtn = document.querySelector('#getInfo');
 const select = document.querySelector('select');
 
-getInfoBtn.addEventListener('click', async () => {
-    removeActive();
-    const film = await getElementOf('films', select.value);
-    // let allCharacters = [];
+
+async function getCharacters(num) {
+  load.style.display = 'block';
+  try {
+    const result = await getJSONResponse(`https://swapi.dev/api/films/${num}`);
     root.textContent = '';
+    load.style.display = 'none';
 
-    film.characters.forEach(character => {
-        let shortInfo = getPage(character)
-            .then(response => {
-                let div = document.createElement('div');
-                div.classList.add('item');
-                let h2 = document.createElement('h2');
-                let p1 = document.createElement('p');
-                let p2 = document.createElement('p');
-                const img =document.createElement('img');
-                const id = parseInt((response.url).replace(/\D+/g,""));
-                img.setAttribute('src', `./img/${id}.jpg`);
-                img.setAttribute('alt', 'avatar');
-                img.setAttribute('class', 'avatar');
+    result.characters.forEach(async function (each) {
+      const person = await getJSONResponse(each);
+      const id = parseInt((person.url).replace(/\D+/g, ""));
+      const data = `
+        <div class="item">
+          <h2>${person.name}</h2>
+          <img class="avatar" src="./img/${id}.jpg" alt="avatar">
+          <p><b>Birthday:</b> ${person.birth_year}</p>
+          <p><b>Gender:</b> ${person.gender === 'male' ? String.fromCodePoint('0x2642') :
+          person.gender === 'female' ? String.fromCodePoint('0x2640') :
+          String.fromCodePoint('0x26A7')}</p>
+        </div>
+      `;
 
-                h2.textContent = response.name;
-                p1.textContent = `Birthday: ${response.birth_year}`;
-                p2.textContent = `Gender: ${(response.gender === 'male') ? String.fromCodePoint('0x2642') :
-                    (response.gender === 'female') ? String.fromCodePoint('0x2640') :
-                    String.fromCodePoint('0x26A7')}`;
-
-                div.append(h2, img, p1, p2);
-                root.append(div);
-            });
+      root.innerHTML += data;
     });
-
-});
-
-
-
-///////////////////////////////////////////
-// Показати список всіх елементів у консоль
-
-const getAllInfo = document.querySelector('.nav');
-
-getAllInfo.addEventListener('click', async (e) => {
-    let target = e.target.dataset.key;
-    console.log(await getAllElementsOf(target));
-
-});
-
-
-////////////////
-// Переключання між вкладками і рендеринг відповідних даних
-// Завдання 2
-
-const navlinks = document.querySelectorAll(".nav_link");
-const nav = document.querySelector(".nav");
-
-function removeActive() {
-    for (let i = 0; i < navlinks.length; i++) {
-        navlinks[i].className = navlinks[i].className.replace("active", "");
-    }
+  } catch (err) {
+    console.warn(err);
+  }
 }
 
-function showContent(index = 0) {
-    navlinks[index].classList.add("active");
-}
-
-removeActive();
-showContent();
-
-nav.addEventListener('click', (event) => {
-    event.preventDefault();
-    navlinks.forEach((item, i) => {
-
-        if (item === event.target) {
-            removeActive();
-            showContent(i);
-            render(`https://swapi.dev/api/${event.target.dataset.key}/?page=1`);
-        }
-    });
+getInfoBtn.addEventListener('click', () => {
+  getCharacters(select.value);
 });
 
-render('https://swapi.dev/api/films/?page=1');
 
-/////////////////////////////////////////////////////////////
-// Створення кнопок next та previous для перемикання сторінок
-// Завдання 3
+////////////////////////////////
 
 const nextBtn = document.querySelector('.next');
-const previousBtn = document.querySelector('.previous');
+const prevBtn = document.querySelector('.previous');
 
-nextBtn.addEventListener('click', (e) => {
-    e.preventDefault();
+async function getPlanet(url) {
+  try {
+    load.style.display = 'block';
+    const data = await getJSONResponse(url);
+    nextBtn.dataset.url = data.next ? data.next : '';
+    prevBtn.dataset.url = data.previous ? data.previous : '';
+    root.textContent = '';
+    load.style.display = 'none';
 
-    if (curPage !== null) {
-        render(curPage);
-    }
+    data.results.forEach(each => {
+      const data = `
+        <div class="item">
+          <h2>${each.name}</h2>
+          <p><b>Climate:</b> ${each.climate}</p>
+          <p><b>Population:</b> ${each.population}</p>
+        </div>
+      `;
+
+      root.innerHTML += data;
+    });
+  } catch (err) {
+    console.warn(err);
+  }
+}
+
+///////////// control buttons
+
+nextBtn.addEventListener('click', async (e) => {
+  if (e.target.dataset.url) {
+    getPlanet(e.target.dataset.url);
+  }
 });
 
-previousBtn.addEventListener('click', (e) => {
-    e.preventDefault();
+prevBtn.addEventListener('click', async (e) => {
+  if (e.target.dataset.url) {
+    getPlanet(e.target.dataset.url);
+  }
+});
 
-    if (prevPage !== null) {
-        render(prevPage);
-    }
+const planetsBtn = document.querySelector('.planets');
+
+planetsBtn.addEventListener('click', () => {
+  getPlanet('https://swapi.dev/api/planets/?page=1');
+});
+
+///////////////
+
+const nav = document.querySelector('.nav');
+
+document.addEventListener('scroll', () => {
+  if (nav.offsetTop > 148) {
+    nav.style.background = 'black';
+  } else {
+    nav.style.background = 'none';
+  }
 });
