68d67a4fb67b96c2bd25ecf10dc173cb72067d52
Bohdan Omelianets <omelianecb@gmail.com>
2021-07-25 20:14:39 +0300

    work with webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
 import './style.css';
 import logo from './logo.png';
-import {numberInPower} from '../homework-03/homework-03';
+import {
+    getRandomPassword,
+    filterEvenNumbers,
+    students,
+    getStudentsNames,
+    Student,
+    getRandomColor,
+    newFontGenerator,
+    getRandomChinese
+        } from './homework-16';
 
+console.log(`Homework 3: ${getRandomPassword(4)}`);
+console.log(`Homework 5: ${filterEvenNumbers(6, 42, 51, 3, 18)}`);
+console.log(`Homework 6: ${getStudentsNames(students)}`);
+const student = new Student('NULP', 4, 'Iron Man');
+console.log(`Homework 8: `);
+student.getInfo();
+console.log(`Homework 9: ${ getRandomColor()}`);
 
+const fontGenerator = newFontGenerator(14);
+console.group('Font generator, Homework 14');
+console.log(fontGenerator.next().value);
+console.log(fontGenerator.next("up").value);
+console.log(fontGenerator.next().value);
+console.log(fontGenerator.next("down").value);
+console.log(fontGenerator.next("down").value);
+console.groupEnd('End');
 
-console.log(numberInPower(2, 3));
+// (async function load() {
+//     const {getRandomChinese} = await import('./homework-16');
+//     console.log(`Homework 11: ${await getRandomChinese(4)}`);
+// })();
+
+// console.log(`Homework 11: ${res}`);
+getRandomChinese(4)
+.then(res => {
+    console.log(`Homework 11: ${res}`);
+});
